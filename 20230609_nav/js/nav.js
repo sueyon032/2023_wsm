49796b367739ed0d8639c45ed69e0f73b6008ad2
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-06-20 10:59:01 +0900

    nav js, sytle.css->nav.css

--- 20230609_nav/js/nav.js
+++ 20230609_nav/js/nav.js
@@ -14,10 +14,15 @@ const navToggleI= nav_toggle.getElementsByTagName("i")[0];
 const navListUl= document.getElementsByClassName("nav-list")[0];
 
 // 2. Event Handling
-// 햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bit-list" <-> X로 "bi bi-x-lg"
-// 햄버거메뉴 클릭하면, .nav-list에 .show-menu 보여주기 <-> 안보여주기
-nav_toggle.onclick= ()=> alert("안녕"); // ok
+// nav_toggle.onclick= ()=> alert("안녕"); // ok
 // nav_toggle.onclick= alert("안녕"); // not ok
+nav_toggle.onclick=()=>{
+    // 햄버거메뉴 클릭하면, 햄버거메뉴 i "bi bit-list" <-> X로 "bi bi-x-lg"
+    navToggleI.classList.toggle("bi-list");
+    navToggleI.classList.toggle("bi-x-lg");
+    // 햄버거메뉴 클릭하면, .nav-list에 .show-menu 보여주기 <-> 안보여주기
+    navListUl.classList.toggle("show-menu");
+}
 
 //--------------------------------------------------------------------
 // HTML - > js
